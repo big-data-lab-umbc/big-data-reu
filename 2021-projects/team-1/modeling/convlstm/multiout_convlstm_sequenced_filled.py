be80a61561c5281defbff86b58dcd0859006c8a5
webzerg <huangxin0921@gmail.com>
2022-02-23 14:24:10 -0500

    add transformer

--- 2021-projects/team-1/modeling/convlstm/multiout_convlstm_sequenced_filled.py
+++ 2021-projects/team-1/modeling/convlstm/multiout_convlstm_sequenced_filled.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from tensorflow import keras 
 from tensorflow.compat.v1.keras import backend as K
 from sklearn.metrics import mean_squared_error
+from tensorflow.keras import layers
 
 '''
 Author: Peter I. Kruse
@@ -41,6 +42,125 @@ def custom_mse(y_true, y_pred):
 	mse = tf.reduce_mean(squared_resids)
 	return mse
 
+# DATA
+#DATASET_NAME = "organmnist3d"
+BATCH_SIZE = 32
+AUTO = tf.data.AUTOTUNE
+# INPUT_SHAPE = (28, 28, 28, 1)
+INPUT_SHAPE = (12, 448, 304, 11)
+NUM_CLASSES = 11
+
+# OPTIMIZER
+LEARNING_RATE = 1e-4
+WEIGHT_DECAY = 1e-5
+
+# TRAINING
+EPOCHS = 10
+
+# TUBELET EMBEDDING
+PATCH_SIZE = (2, 8, 8)
+NUM_PATCHES = (INPUT_SHAPE[0] // PATCH_SIZE[0]) ** 2
+print(INPUT_SHAPE[0] // PATCH_SIZE[0])
+print("NUM_PATCHES:", NUM_PATCHES)
+
+# ViViT ARCHITECTURE
+LAYER_NORM_EPS = 1e-6
+PROJECTION_DIM = 128
+NUM_HEADS = 8
+NUM_LAYERS = 8
+
+class TubeletEmbedding(layers.Layer):
+    def __init__(self, embed_dim, patch_size, **kwargs):
+        super().__init__(**kwargs)
+        self.projection = layers.Conv3D(
+            filters=embed_dim,
+            kernel_size=patch_size,
+            strides=patch_size,
+            padding="VALID",
+        )
+        self.flatten = layers.Reshape(target_shape=(-1, embed_dim))
+
+    def call(self, videos):
+        projected_patches = self.projection(videos)
+        flattened_patches = self.flatten(projected_patches)
+        return flattened_patches
+
+class PositionalEncoder(layers.Layer):
+    def __init__(self, embed_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.embed_dim = embed_dim
+
+    def build(self, input_shape):
+        _, num_tokens, _ = input_shape
+        self.position_embedding = layers.Embedding(
+            input_dim=num_tokens, output_dim=self.embed_dim
+        )
+        self.positions = tf.range(start=0, limit=num_tokens, delta=1)
+
+    def call(self, encoded_tokens):
+        # Encode the positions and add it to the encoded tokens
+        encoded_positions = self.position_embedding(self.positions)
+        encoded_tokens = encoded_tokens + encoded_positions
+        return encoded_tokens
+
+def create_transformer(
+    tubelet_embedder,
+    positional_encoder,
+    input_shape=INPUT_SHAPE,
+    transformer_layers=NUM_LAYERS,
+    num_heads=NUM_HEADS,
+    embed_dim=PROJECTION_DIM,
+    layer_norm_eps=LAYER_NORM_EPS,
+    num_classes=NUM_CLASSES,
+):
+    # Get the input layer
+    inputs = layers.Input(shape=input_shape)
+    # Create patches.
+    patches = tubelet_embedder(inputs)
+    # Encode patches.
+    encoded_patches = positional_encoder(patches)
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        # Layer normalization and MHSA
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embed_dim // num_heads, dropout=0.1
+        )(x1, x1)
+
+        # Skip connection
+        x2 = layers.Add()([attention_output, encoded_patches])
+
+        # Layer Normalization and MLP
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        x3 = keras.Sequential(
+            [
+                layers.Dense(units=embed_dim * 4, activation=tf.nn.gelu),
+                layers.Dense(units=embed_dim, activation=tf.nn.gelu),
+            ]
+        )(x3)
+
+        # Skip connection
+        encoded_patches = layers.Add()([x3, x2])
+
+    # Layer normalization and Global average pooling.
+    representation = layers.LayerNormalization(epsilon=layer_norm_eps)(encoded_patches)
+    representation = layers.GlobalAvgPool1D()(representation)
+
+    # Classify outputs.
+    # outputs = layers.Dense(units=num_classes, activation="softmax")(representation)
+
+    # Create the Keras model.
+    # model = keras.Model(inputs=inputs, outputs=outputs)
+    return representation
+
+transformer = create_transformer(
+			tubelet_embedder=TubeletEmbedding(
+				embed_dim=PROJECTION_DIM, patch_size=PATCH_SIZE
+			),
+			positional_encoder=PositionalEncoder(embed_dim=PROJECTION_DIM)
+		)
+
 # make multi output model
 # define model class
 class MultiOutputConvLSTM():
@@ -48,12 +168,13 @@ class MultiOutputConvLSTM():
 	def make_default_hidden_layers(self, inputs):
 	# this method makes the default hidden layers, which both branches of the network will utilize
 	# ConvLSTM2d -> MaxPooling3D -> ConvLSTM2D -> Conv2D -> Flatten -> Dense
-		x = keras.layers.ConvLSTM2D(8, (5,5), padding="same", return_sequences=False)(inputs)
-		x = keras.layers.MaxPooling2D((4, 4))(x)
-		x = keras.layers.Conv2D(128, (5,5), activation="relu")(x)
-		x = keras.layers.MaxPooling2D((4,4))(x)
-		x = keras.layers.Conv2D(32, (5,5), activation="relu")(x)
-		x = keras.layers.Flatten()(x)
+		# x = keras.layers.ConvLSTM2D(8, (5,5), padding="same", return_sequences=False)(inputs)
+		# x = keras.layers.MaxPooling2D((4, 4))(x)
+		# x = keras.layers.Conv2D(128, (5,5), activation="relu")(x)
+		# x = keras.layers.MaxPooling2D((4,4))(x)
+		# x = keras.layers.Conv2D(32, (5,5), activation="relu")(x)
+		# x = keras.layers.Flatten()(x)
+		x = transformer(inputs)
 		x = keras.layers.Dense(256, activation="relu")(x)
 		return x
 
